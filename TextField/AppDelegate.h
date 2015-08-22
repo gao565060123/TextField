fd4bb17432d253f96fba54a95679a7011f492034
MS <ms@zhou.local>
2015-08-22 14:49:11 +0800

    Initial Commit

---
+++ TextField/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TextField
+//
+//  Created by MS on 15-8-22.
+//  Copyright (c) 2015年 zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
